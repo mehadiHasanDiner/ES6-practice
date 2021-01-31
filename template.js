db18a337ee3660e4f42981d912092196a454eeaa
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-02-01 02:40:07 +0600

    ES6 javaScript

---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName = "Mehadi";
+const lastName = "hasan";
+
+const fullName = `${firstName} ${50+20+30} is a good boy`;
+
+const multiLine = `I miss yoo
+i need you
+dont need you`
+console.log(multiLine);
